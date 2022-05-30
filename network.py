be02bb55aa7085a1e89aad796ff94f221c507e4e
Guilherme Ferreira <guifs2@hotmail.com>
2022-05-30 17:33:18 -0300

    add network class

---
+++ network.py
@@ -0,0 +1,10 @@
+class Network:
+    def __init__(self, size_vertices, size_edges, demand, adjacent_matrix, weight, cappacity):
+        self.size_vertices = size_vertices
+        self.size_edges = size_edges
+        self.demand = demand
+        self.adjacent_matrix = adjacent_matrix
+        self.weight = weight
+        self.cappacity = cappacity
+
+    
