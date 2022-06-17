07edc1ff1ddea90acfa1f408c22a9017bfc12078
luisabrito <90068390+luisabritod@users.noreply.github.com>
2022-06-17 10:41:21 -0300

    new att

--- network.py
+++ network.py
@@ -2,11 +2,11 @@ from csv import reader
 
 
 class Network:
-    def __init__(self, size_vertices=0, adjacent_list=None, adjacent_matrix=None, arestas_list=None, cappacity=0, cost=0, demand=None):
+    def __init__(self, size_vertices=0, adjacent_list=None, adjacent_matrix=None, size_arestas=0, arestas_list=None, cappacity=0, cost=0, demand=None):
         self.size_vertices = size_vertices
-        self.dict = {}
-        self.s = 0
-        self.t = 0
+        self.size_arestas = size_arestas
+        self.dict_prof = {}
+        self.dict_disc = {}
         self.lista_professores = []
         self.lista_disciplinas = []
 
@@ -91,6 +91,13 @@ class Network:
         else:
             print("Aresta invalida!")
 
+    def add_lista_arestas(self):
+        for i in range(len(self.adjacent_matrix)):
+            for j in range(len(self.adjacent_matrix[i])):
+                if self.adjacent_matrix[i][j] != 0:
+                    w = self.adjacent_matrix[i][j]
+                    self.arestas_list.append((i, j, w))
+
     def add_Dictionary(self, value, key):
         self.dict[value] = key
 
@@ -149,22 +156,58 @@ class Network:
         self.demand.append(self.s)
         self.demand.append(self.t)
 
+    def juntar_professores_disciplinas(self, disc_informacoes, dados_prof, vert_inicial):
+        (prof, numero_disc, disc_oferecidas) = dados_prof
+
+        for i in range(len(prof)):
+            self.dict_prof[i+1] = (prof[i], numero_disc[i], disc_oferecidas[i])
+
+        for i in range(vert_inicial, self.size_vertices - 1):
+            for j in disc_informacoes:
+                self.dict_disc[i] = j
+                disc_informacoes.remove(j)
+                break
+
+    def lista_de_professores(self, prof, disc):
+        const = self.adjacent_matrix[0]
+        copia = [0]
+        copia = copia + disc.copy()
+
+        for i in range(len(prof+1)):
+            cont = i
+            capac = copia[i]
+            self.add_aresta(const[i], cont, capac)
+
+        self.adjacent_matrix[0][0] = 0
+        self.adjacent_list[0].pop(0)
+
     def bellman_ford(self, s, t):
         dist = [float("inf")
-                for _ in range(self.size_vertices)]  # Distance from s
+                for _ in range(len(self.adjacent_list))]  # Distance from s
         # Predecessor in shortest path from s
-        pred = [None for _ in range(self.size_vertices)]
+        pred = [None for _ in range(len(self.adjacent_list))]
         dist[s] = 0
-        for i in range(self.size_vertices):
+        for i in range(self.adjacent_list-1):
             updated = False
             for (u, v, w) in self.arestas_list:
                 if dist[v] > dist[u] + w:
                     dist[v] = dist[u] + w
                     pred[v] = u
                     updated = True
-            if not updated:
-                return dist, pred
-        return dist, pred
+            if updated is False:
+                break
+
+        caminho = []
+        i = float('inf')
+        caminho.append(t)
+
+        while i != s:
+            i = pred[t]
+            t = i
+            caminho.append(i)
+        caminho.reverse()
+
+        return caminho
 
     def SucessivosCaminhosMinimos(self, s, t):
         F = [[0 for i in range(len(self.size_vertices))]
